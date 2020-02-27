70c98b2a8fe36fbc7d0766853e76993692309d90
Wajahat Raza <48519169+ntrov@users.noreply.github.com>
2020-02-27 10:52:54 +0500

    Add files via upload

---
+++ assignments/15-puzzle/prog.py
@@ -0,0 +1,10 @@
+class fifteenPuzzle:
+    
+    def hello(self):
+        '''Prints hello world'''
+        print("Hello World!!")
+
+
+if __name__ == "__main__":
+    agent = fifteenPuzzle()
+    agent.hello()
